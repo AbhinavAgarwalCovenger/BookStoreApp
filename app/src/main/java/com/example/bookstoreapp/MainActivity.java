084b983c63757424b3487035d5f1bd10811f16b6
rohitsahay <rohitsahay.199626@gmail.com>
2020-01-19 13:54:38 +0530

    main updated

--- app/src/main/java/com/example/bookstoreapp/MainActivity.java
+++ app/src/main/java/com/example/bookstoreapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.bookstoreapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    Button searchBtn;
+    Button cartBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        searchBtn = (Button) findViewById(R.id.search_btn);
+        cartBtn = (Button) findViewById(R.id.cart_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login Activity will open", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Search Activity wll open", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        cartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Cart Activity will open", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
