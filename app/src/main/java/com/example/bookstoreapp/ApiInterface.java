54eeb1871f12e9c7bf9da4b36eac7b9a71709e7b
Abhinav <abhinav.agarwal@coviam.com>
2020-01-21 12:03:33 +0530

    integrated login api with backend

---
+++ app/src/main/java/com/example/bookstoreapp/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.example.bookstoreapp;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+//    @GET("bins/aa0hm")
+//    Call<List<Books>> getBooks();
+
+//      @POST("getCustomer")
+//      Call<Customer> getCustomer();
+
+      @POST("login")
+      Call<Customer> getCustId(@Body Customer customer);
+}
--- app/src/main/java/com/example/bookstoreapp/SignUpApi.java
+++ app/src/main/java/com/example/bookstoreapp/SignUpApi.java
@@ -10,7 +10,7 @@ import retrofit2.http.POST;
 
 public interface SignUpApi {
 
-    @POST("customer/signup")
+    @POST("signup")
     Call<ResponseBody>  createUser(
             @Body Customer customer
 //      @Field("name") String name,
